9a6002ba55ea3663e0b38411c94988aea3ee01c4
Dimitris Christodoulou <s1738739@ed.ac.uk>
2019-10-22 18:25:08 +0100

    Use Mapbox Geo-JSON API

--- src/main/java/uk/ac/ed/inf/powergrab/GeoJSON.java
+++ src/main/java/uk/ac/ed/inf/powergrab/GeoJSON.java
@@ -1,50 +1,56 @@
 package uk.ac.ed.inf.powergrab;
 
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.Point;
+import com.mapbox.geojson.LineString;
 import com.google.gson.JsonParser;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonArray;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.net.MalformedURLException;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 
 public class GeoJSON {
-	private final JsonObject featureCollection;
+	private final FeatureCollection fc;
 	private Map<Position, Station> stations = new HashMap<Position, Station>();
 	
-	public GeoJSON(String year, String month, String day) throws MalformedURLException, IOException {
-		URL url = new URL(String.format("http://homepages.inf.ed.ac.uk/stg/powergrab/%s/%s/%s/powergrabmap.geojson", year, month, day));
-		URLConnection connection = url.openConnection();
-		connection.connect();
+	public GeoJSON(String year, String month, String day) throws IOException {
+		String mapString = String.format("http://homepages.inf.ed.ac.uk/stg/powergrab/%s/%s/%s/powergrabmap.geojson", year, month, day);
+		URL url = new URL(mapString);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setReadTimeout(10000);
+		conn.setConnectTimeout(15000);
+		conn.setRequestMethod("GET");
+		conn.setDoInput(true);
+		conn.connect();
 			
-		InputStream is = connection.getInputStream();
-		InputStreamReader isR = new InputStreamReader(is);
+		InputStream is = conn.getInputStream();
+		InputStreamReader isReader = new InputStreamReader(is);
 		JsonParser parser = new JsonParser();
-		JsonElement root = parser.parse(isR);
-		this.featureCollection = root.getAsJsonObject();
-		
+		JsonElement root = parser.parse(isReader);
+		String mapSource = root.toString();
+		this.fc = FeatureCollection.fromJson(mapSource);
+		System.out.println(mapSource.length());
+
 		// Fill the stations map
-		JsonArray features = featureCollection.get("features").getAsJsonArray();
-		for (JsonElement feature : features) {
-			JsonObject featureObject = feature.getAsJsonObject();
-			JsonObject properties = featureObject.get("properties").getAsJsonObject();
-			double coins = properties.get("coins").getAsDouble();
-			double power = properties.get("power").getAsDouble();
-			JsonArray coordinates = featureObject.get("geometry").getAsJsonObject().get("coordinates").getAsJsonArray();
-			double longitude = coordinates.get(0).getAsDouble();
-			double latitude = coordinates.get(1).getAsDouble();
+		for (Feature f : fc.features()) {
+			double coins = f.getProperty("coins").getAsDouble();
+			double power = f.getProperty("power").getAsDouble();
+			Point p = (Point) f.geometry();
+			List<Double> coordinates = p.coordinates();
+			double longitude = coordinates.get(0);
+			double latitude = coordinates.get(1);
 			Position pos = new Position(latitude, longitude);
 			this.stations.put(pos, new Station(pos, coins, power));
 		}
-		System.out.println(featureCollection);
 	}
 	
 	public Map<Position, Station> getStations(){
@@ -53,27 +59,18 @@ public class GeoJSON {
 	
 	
 	public void addPathToJSON(List<Position> pathPositions) {
-		JsonArray features = featureCollection.get("features").getAsJsonArray();
-		JsonObject lineString = new JsonObject();
-		lineString.addProperty("type", "Feature");
-		lineString.add("properties", new JsonObject());
-		JsonObject geometry = new JsonObject();
-		geometry.addProperty("type", "LineString");
-		JsonArray coordinates = new JsonArray();
-		geometry.add("coordinates", coordinates);
+		ArrayList<Point> pointsList = new ArrayList<Point>();
 		for (Position pos : pathPositions) {
-			JsonArray positionCoords = new JsonArray();
-			positionCoords.add(pos.longitude);
-			positionCoords.add(pos.latitude);
-			coordinates.add(positionCoords);
+			pointsList.add(Point.fromLngLat(pos.longitude, pos.latitude));
 		}
-		lineString.add("geometry", geometry);
-		features.add(lineString);
+		LineString flightPath = LineString.fromLngLats(pointsList);
+		Feature f = Feature.fromGeometry(flightPath);
+		fc.features().add(f);
 	}
 	
 	public void printGeoJsonToFile() throws FileNotFoundException {
 		PrintWriter output = new PrintWriter("output.geojson");
-		output.println(featureCollection.toString());
+		output.println(fc.toJson());
 		output.close();
 	}
 	
