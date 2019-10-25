5f1c4d7c516077167b63b79ff4531b712da7f058
Dimitris Christodoulou <s1738739@ed.ac.uk>
2019-10-25 13:10:03 +0100

    Add Move class to handle moves

--- src/main/java/uk/ac/ed/inf/powergrab/Move.java
+++ src/main/java/uk/ac/ed/inf/powergrab/Move.java
@@ -1,4 +1,62 @@
 package uk.ac.ed.inf.powergrab;
 
 public class Move {
+    private Position positionBefore;
+    private Direction moveDirection;
+    private Position positionAfter = null;
+    private Double coinsAfter = null;
+    private Double powerAfter = null;
+    private Drone droneToMove = null;
+    private Game game = null;
+    private Station connectedStation = null;
+    private static final double POWER_CONSUMPTION = 1.25;
+
+    public Move(Game game, Drone droneToMove, Direction moveDirection) {
+        this.positionBefore = droneToMove.getPosition();
+        this.moveDirection = moveDirection;
+        this.droneToMove = droneToMove;
+        this.game = game;
+    }
+
+    public void move() {
+        this.positionAfter = this.positionBefore.nextPosition(moveDirection);
+        this.droneToMove.position = this.positionAfter;
+        this.connectedStation = this.game.getConnectedStation(this.positionAfter);
+        if (this.connectedStation != null) {
+            this.connectedStation.connect(droneToMove);
+        }
+        droneToMove.power -= POWER_CONSUMPTION;
+        this.powerAfter = droneToMove.getPower();
+        this.coinsAfter = droneToMove.getCoins();
+    }
+
+    public Station getConnectedStation() {
+        return this.connectedStation;
+    }
+
+    public String toString() {
+        return this.positionBefore.latitude + "," +
+                this.positionBefore.longitude + "," +
+                this.moveDirection + "," +
+                this.positionAfter.latitude + "," +
+                this.positionAfter.longitude + "," +
+                this.coinsAfter + "," +
+                this.powerAfter;
+    }
+
+    public Position getPositionAfter() {
+        return positionAfter;
+    }
+
+    public Position getPositionBefore() {
+        return positionBefore;
+    }
+
+    public double getCoinsAfter() {
+        return coinsAfter;
+    }
+
+    public double getPowerAfter() {
+        return powerAfter;
+    }
 }
--- src/main/java/uk/ac/ed/inf/powergrab/StatefulDrone.java
+++ src/main/java/uk/ac/ed/inf/powergrab/StatefulDrone.java
@@ -2,7 +2,6 @@ package uk.ac.ed.inf.powergrab;
 
 import java.util.List;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Collections;
 import java.util.Queue;
 import org.apache.commons.collections4.queue.CircularFifoQueue;
@@ -12,15 +11,15 @@ public class StatefulDrone extends Drone {
 	private static final int NUM_RECENT_POSITIONS = 5;
 	private Queue<Position> recentPositions;
 
+
 	public StatefulDrone(Position position, long randomSeed) {
 		super(position, randomSeed);
 		this.recentPositions = new CircularFifoQueue<Position>(NUM_RECENT_POSITIONS);
 		this.recentPositions.add(this.position);
 	}
 	
-	public boolean nextMove(Station targetStation) {
+	public Move nextMove(Station targetStation) {
 		// Next move is the reachable position that is closer to the target station
-		boolean reachedTarget = false;
 		ArrayList<Direction> bestDirections = new ArrayList<Direction>();
 		double minDistance = Double.MAX_VALUE;
 		for (Direction d : Direction.values()) {
@@ -47,37 +46,26 @@ public class StatefulDrone extends Drone {
 		// Direction moveDirection = bestDirections.get(this.rnd.nextInt(bestDirections.size()));
 		Collections.shuffle(bestDirections, this.rnd);
 		Direction moveDirection = bestDirections.get(0);
-		Position nextPosition = this.position.nextPosition(moveDirection);
-		this.position = nextPosition;
-		Station connectedStation = this.game.getConnectedStation(this.position);
-		if (connectedStation != null) {
-			if (connectedStation.equals(targetStation)) {
-				reachedTarget = true;
-			}
-			connectedStation.connect(this);
-		}
-		this.power -= POWER_CONSUMPTION;
-		return reachedTarget;
+		Move move = this.move(moveDirection);
+		this.moveHistory.add(move);
+		this.recentPositions.add(this.position);
+		return move;
 	}
 	
-	public List<Position> planPath() {
+	public void planPath() {
 		int numMoves = 0;
-		ArrayList<Position> path = new ArrayList<Position>();
-		path.add(this.position);
 		Station targetStation = this.game.getNearestPositiveStation(this.position);
 		while (this.power >= POWER_CONSUMPTION && numMoves < 250) {
 			numMoves++;
-			boolean reachedTarget = this.nextMove(targetStation);
-			if (reachedTarget)
+			Move move = this.nextMove(targetStation);
+			// check if target has been reached
+			Station connectedStation = move.getConnectedStation();
+			if ((connectedStation != null) && (connectedStation.equals(targetStation)))
 				targetStation = this.game.getNearestPositiveStation(this.position);
-			this.recentPositions.add(this.position);
-			path.add(this.position);
 		}
 		if (this.power < POWER_CONSUMPTION) {
 			System.out.println("Out of power!");
 		}
 		System.out.println(this.coins + " Coins Collected");
-		return path;
 	}
-
 }
