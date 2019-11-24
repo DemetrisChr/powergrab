ad13f5aea98b5e03b7f5cf6df068b33dfd3eb851
Dimitris Christodoulou <s1738739@ed.ac.uk>
2019-11-24 16:08:07 +0000

    Comments & small changes to exception handling

--- src/main/java/uk/ac/ed/inf/powergrab/App.java
+++ src/main/java/uk/ac/ed/inf/powergrab/App.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 public class App {
     public static void main( String[] args ) {
-        // Reading Arguments
+        // Read arguments
         String day = args[0];
         String month = args[1];
         String year = args[2];
@@ -13,29 +13,32 @@ public class App {
         double longitude = Double.parseDouble(args[4]);
         long randomSeed = Integer.parseInt(args[5]);
         String droneType = args[6];
+
         // Run the simulation
         try {
             runSimulation(day, month, year, latitude, longitude, randomSeed, droneType);
+        } catch (FileNotFoundException e) {
+            System.out.println("Save directory for output files does not exist."); return;
         } catch (IOException e) {
-            System.out.println("Error reading map");
-            return;
+            System.out.println("Unable to retrieve game map."); return;
         }
     }
 
-    public static String runSimulation(String day, String month, String year, double latitude, double longitude, long randomSeed, String droneType) throws IOException {
-        GameMap gameMap = new GameMap(year, month, day);
+    public static String runSimulation(String day, String month, String year, double latitude, double longitude, long randomSeed, String droneType) throws IOException, FileNotFoundException {
+        GameMap gameMap = new GameMap(year, month, day); // Load and parse the game map
         Drone drone = (droneType.equals("stateful"))
                 ? new StatefulDrone(new Position(latitude, longitude), gameMap, randomSeed)
                 : new StatelessDrone(new Position(latitude, longitude), gameMap, randomSeed);
-        drone.planPath();
-        gameMap.addDronePathToMap(drone);
+
+        drone.planPath(); // Plan the path of the drone in the map
+        gameMap.addDronePathToMap(drone); // Add the drone's path to the Geo-JSON document
+
+        // Output the Geo-JSON document (with the added drone path) and the drone's move history to files
         String fileName = "./outputs/"+drone.getDroneType()+"-"+day+"-"+month+"-"+year;
-        try {
-            gameMap.outputMapToFile(fileName);
-            drone.outputMoveHistoryToFile(fileName);
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found!");
-        }
+        gameMap.outputMapToFile(fileName);
+        drone.outputMoveHistoryToFile(fileName);
+
+        // Print and return the details of the map alongside the score achieved by the drone and the perfect score
         String result = droneType+","+year+"-"+month+"-"+day+","+drone.getCoins()+","+ gameMap.getPerfectScore();
         System.out.println(result);
         return result;
