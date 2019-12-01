016f11e587706656fc52c5f41cc25e4592f12538
Dimitris Christodoulou <s1738739@ed.ac.uk>
2019-12-01 14:50:14 +0000

    Use factory method to create new Node objects, and implement A* frontier as PriorityQueue

--- src/main/java/uk/ac/ed/inf/powergrab/StatefulDrone.java
+++ src/main/java/uk/ac/ed/inf/powergrab/StatefulDrone.java
@@ -42,21 +42,17 @@ public class StatefulDrone extends Drone {
     }
 
     private LinkedList<Move> nextBatchOfMovesToTarget() {
-        Position startPosition = this.position;
+        Node.newSearch(this.gameMap);
+        PriorityQueue<Node> frontier = new PriorityQueue<Node>(); // Discovered nodes that may need to be expanded
 
-        TreeSet<Node> open = new TreeSet<Node>(); // Set of discovered nodes that may need to be expanded
-        TreeSet<Node> allNodes = new TreeSet<Node>(); // Set of all the nodes that have ever been encountered
-
-        Node rootNode = new Node(startPosition, this.gameMap);
+        Node rootNode = Node.getNodeWithPosition(this.position);
         // Add the root (starting) node to the sets
-        open.add(rootNode);
-        allNodes.add(rootNode);
+        frontier.add(rootNode);
         rootNode.g = 0;
         rootNode.calculateHandF(targetStation);
 
-        while (!open.isEmpty()) {
-            Node current = open.first(); // Current is the node with the lowest f score
-            open.remove(current); // Remove the current node from the open set
+        while (!frontier.isEmpty()) {
+            Node current = frontier.poll(); // Retrieve and remove the node with the lowest f-score
 
             // If the drone can connect to the target station at the current node the goal has been reached
             // and the path is returned
@@ -69,15 +65,6 @@ public class StatefulDrone extends Drone {
 
             for (Direction moveDir : adjacentNodesToCurrent.keySet()) {
                 Node neighbour = adjacentNodesToCurrent.get(moveDir);
-                boolean nodeAlreadyExists = false;
-                // If an equivalent node (same position) has been seen before, use the existing node object instead
-                for (Node n : allNodes)
-                    if (n.equals(neighbour)) {
-                        neighbour = n;
-                        nodeAlreadyExists = true;
-                        break;
-                    }
-                if (!nodeAlreadyExists) allNodes.add(neighbour);
                 double tentativeGscore = GameRules.TRAVEL_DISTANCE + current.g; // Distance from start to neighbour through current
                 if (tentativeGscore < neighbour.g) {
                     // This path to neighbour is better than any previous one recorded
@@ -86,11 +73,13 @@ public class StatefulDrone extends Drone {
                     neighbour.g = tentativeGscore;
                     // Calculate the heuristic (h) and f
                     neighbour.calculateHandF(targetStation);
-                    open.add(neighbour); // As 'open' is a set, if 'open' already contains neighbour it will not be added
+                    // If the neighbour is not already in the frontier, add it
+                    if (!frontier.contains(neighbour))
+                        frontier.add(neighbour);
                 }
             }
         }
-        return null; // Open set is empty and target has not been reached
+        return null; // Frontier is empty and target has not been reached
     }
 
     private LinkedList<Move> reconstructPath(Node current) {
@@ -105,26 +94,40 @@ public class StatefulDrone extends Drone {
     public String getDroneType() { return "stateful"; }
 
     private static class Node implements Comparable<Node> {
-        public Position position;
+        public static GameMap gameMap = null;
+        public static Set<Node> allNodes = new HashSet<Node>();
 
+        public Position position;
         public double g = Double.POSITIVE_INFINITY; // Distance travelled on shortest path to reach node
         public double h = Double.POSITIVE_INFINITY; // Estimated distance to target
         public double f = Double.POSITIVE_INFINITY; // f = g + h
         public Node cameFromNode = null; // Preceding node on the shortest path to this node
         public Direction cameFromDirection = null; // Direction of travel on the shortest path, from previous node to this
-        public GameMap gameMap; // The game where this node is located on
 
-        public Node(Position p, GameMap g) {
+        private Node(Position p) {
             this.position = p;
-            this.gameMap = g;
+        }
+
+        public static void newSearch(GameMap gameMap) {
+            Node.allNodes.clear();
+            Node.gameMap = gameMap;
+        }
+
+        public static Node getNodeWithPosition(Position pos) {
+            Node newNode = new Node(pos);
+            for (Node n : allNodes)
+                if (n.equals(newNode))
+                    return n;
+            allNodes.add(newNode);
+            return newNode;
         }
 
         public int compareTo(Node otherNode) {
-            return Double.compare(this.f, otherNode.f); // The f score is the comparison metric between nodes
+            return Double.compare(this.f, otherNode.f); // Nodes with lower f score are ranked higher
         }
 
         public void calculateHandF(Station targetStation) {
-            Station connectedStation = this.gameMap.getConnectedStation(this.position);
+            Station connectedStation = gameMap.getConnectedStation(this.position);
             double penalty = 1;
             if (connectedStation != null && connectedStation.isNegative())
                 penalty = Math.max(GameRules.MIN_NEGATIVE_STATION_PENALTY,
@@ -138,7 +141,7 @@ public class StatefulDrone extends Drone {
             for (Direction d : Direction.values()) {
                 Position pos = this.position.nextPosition(d);
                 if (pos.inPlayArea())
-                    adjacentNodes.put(d, new Node(this.position.nextPosition(d), this.gameMap));
+                    adjacentNodes.put(d, Node.getNodeWithPosition(this.position.nextPosition(d)));
             }
             return adjacentNodes;
         }
